979f6de9091927666264347151242944a5a2f4a1
Fleur Kelpin <f.kelpin@umcg.nl>
2017-09-08 18:06:05 +0200

    Better names for the cumulative height functions

--- src/components/Class.vue
+++ src/components/Class.vue
@@ -15,15 +15,15 @@
   import Divider from './Divider'
 
   // lines have height 15, dividers have height 1, compartments have padding 7.5
-  const compartmentHeight = compartment => (compartment.lines.length + 1) * 15 + 1
-  const cumulativeHeights = (a, b) => [...a, a[a.length - 1] + b]
+  const height = compartment => (compartment.lines.length + 1) * 15 + 1
+  const cumulativeSum = (a, b) => [...a, a[a.length - 1] + b]
 
   export default {
     props: ['compartments', 'width'],
     name: 'class',
     computed: {
       y () {
-        return this.compartments.map(compartmentHeight).reduce(cumulativeHeights, [1])
+        return this.compartments.map(height).reduce(cumulativeSum, [0])
       }
     },
     components: {
